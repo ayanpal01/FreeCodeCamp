9e0ff91fcb7f54892da5f0fe41daf00b251691a0
ayanpal01 <ayanpal260@gmail.com>
2026-07-23 21:18:13 +0530

    Configure Vercel deployment settings

--- Backend and API/project-filemetadata/index.js
+++ Backend and API/project-filemetadata/index.js
@@ -40,3 +40,5 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+module.exports = app;
